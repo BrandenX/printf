43eff10c0b4d7f5d8cb871bc277cb673f5a637d2
Anuka <anukabrandenelvis@.com>
2023-03-28 20:59:56 -0700

    group_printf

--- get_precision.c
+++ get_precision.c
@@ -1,4 +1,3 @@
-
 #include "main.h"
 
 /**
@@ -14,13 +13,10 @@ int get_precision(const char *format, int *i, va_list list)
 	int curr_i = *i + 1;
 	int precision = -1;
 
-
 	if (format[curr_i] != '.')
 		return (precision);
 
-
 	precision = 0;
-
 
 	for (curr_i += 1; format[curr_i] != '\0'; curr_i++)
 	{
@@ -39,9 +35,8 @@ int get_precision(const char *format, int *i, va_list list)
 			break;
 	}
 
-
 	*i = curr_i - 1;
-
 
 	return (precision);
 }
+
